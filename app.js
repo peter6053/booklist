073ec844f61e7b88acaf5bbbd3811e2ed816875e
unknown <you@example.com>
2020-05-15 13:10:15 +0300

    bookstore

---
+++ app.js
@@ -0,0 +1,63 @@
+//bookclass:represent a book
+class book{
+    constructor(title,author,isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+//ui clsaa: handle ui class
+class ui {
+      static displaybooks()
+
+    {
+        title:'bookone'
+        author: 'john doe,'
+        isbn: '123456'
+
+    };
+    {
+const books = storedbooks
+books.forEach(book =>  ui.addbooktolist(book){
+    
+}
+ static addbooktolist(book));
+{
+    const list = document.querySelector('#book-list');
+    const row = document.createElementNS('tr'),
+
+    row.innerhtml= 
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td>${book.title}</td>
+
+        <td> <a href="#" class ="btn btn-danger btn-sm delete"x> </a> 
+        </td>;
+        list.appendChild(row);
+}
+static deletebook(el)
+}
+static clearfields(){
+document.querySelector
+
+}
+
+
+
+//storeclass:handles storoge
+
+//add a nwe boook
+document.querySelector('#title').Value;
+document.querySelector('#author').Value;
+document.querySelector('#isbn').Value;
+
+//initialize a book
+const book = new book(title,author,isbn);
+console.log(book)
+//events:display books
+
+//event:Ada book
+
+//event remoe a book
+
